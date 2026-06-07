8caf4c694dbaf196447c55fc662ee61fb26f7935
Nitesh511 <nitesh.karki31@gmail.com>
2026-06-07 10:27:40 +0545

    commit for the day01

---
+++ day01.py
@@ -0,0 +1,18 @@
+print("Day 1 Started")
+
+#Variables
+
+name= "Nitesh"
+goal= "Become a Data Scientist"
+days_remaning=60
+
+print(f"my name is {name} and my goal is to {goal}. I have only {days_remaning} days left to achieve my goal")
+
+#list 
+
+topics= ["Python", "Data Science", "Machine Learning", "Deep Learning", "NLP"]
+
+print(f"These are the topics I will be learning in the next {days_remaning} days: {topics}")
+
+for n in topics:
+    print(f"I will learn {n} in the next {days_remaning} days")
